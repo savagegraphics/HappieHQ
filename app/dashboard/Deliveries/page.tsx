f3655d191a1e444e8823ac363bd66b17aeca3d1e
savagegraphics <olawaleraji188@gmail.com>
2024-01-18 16:25:54 -0800

    first commit

--- app/dashboard/Deliveries/page.tsx
+++ app/dashboard/Deliveries/page.tsx
@@ -1,24 +1,32 @@
+// import React from 'react'
+// import Router from './Router'
+// import NavSearch from './NavSearch'
+// import TheCard from './TheCard'
+// import Grid from './Grid'
+// import SearchNav from './SearchNav'
+// import Navig from './Navig'
+// import One from '../TailwindComp/One'
+// import Two from '../TailwindComp/Two'
+
+// type Props = {}
+
+// const page = (props: Props) => {
+//   return (
+//     <div className='-mt-8'>
+//       {/* <Two />
+//       <One /> */}
+//       <SearchNav />
+//       <Router />
+//     </div>
+//   )
+// }
+
+// export default page
+
 import React from 'react'
-import Router from './Router'
-import NavSearch from './NavSearch'
-import TheCard from './TheCard'
-import Grid from './Grid'
-import SearchNav from './SearchNav'
-import Navig from './Navig'
-import One from '../TailwindComp/One'
-import Two from '../TailwindComp/Two'
 
 type Props = {}
 
 const page = (props: Props) => {
-  return (
-    <div className='-mt-8'>
-      {/* <Two />
-      <One /> */}
-      <SearchNav />
-      <Router />
-    </div>
-  )
+  return <div>page </div>
 }
-
-export default page
