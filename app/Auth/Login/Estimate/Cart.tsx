eec14172308369a24cd4664d0f94a4591f0f30de
savagegraphics <olawaleraji188@gmail.com>
2024-01-19 17:18:45 -0800

    first commit

---
+++ app/Auth/Login/Estimate/Cart.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Carty from './Cart/page'
+
+type Props = {}
+
+const Cart = (props: Props) => {
+  return (
+    <div>
+      <Carty />
+    </div>
+  )
+}
+
+export default Cart
---
+++ app/dashboard/Estimate/Checkout/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Checks from '../Checks'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <div>
+      <Checks />
+    </div>
+  )
+}
+
+export default page
