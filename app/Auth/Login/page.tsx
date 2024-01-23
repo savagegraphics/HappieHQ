5f0808f6560eacf9f11a975cad786d061bd1da5b
savagegraphics <olawaleraji188@gmail.com>
2024-01-23 04:09:28 -0800

    first commit

--- app/Auth/Login/page.tsx
+++ app/Auth/Login/page.tsx
@@ -39,9 +39,9 @@ const login = (props: Props) => {
                         Sign in
                       </h1>
                       <p className='mt-2 text-sm text-gray-600 dark:text-gray-400'>
-                        Don't have an account yet?
+                        Don&apos;t have an account yet?
                         <a
-                          className='text-blue-600 decoration-2 hover:underline font-medium dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600'
+                          className='text-blue-600 d66 ecoration-2 hover:underline font-medium dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600'
                           href='../examples/html/signup.html'
                         >
                           Sign up here
