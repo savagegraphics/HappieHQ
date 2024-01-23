a1c18a16c937ffc3140865deb81534fca2c48ff3
savagegraphics <olawaleraji188@gmail.com>
2024-01-23 07:13:17 -0800

    first commit

--- app/dashboard/Wallet/page.tsx
+++ app/dashboard/Wallet/page.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Component from '../Components/page'
 
 type Props = {}
 
 const page = (props: Props) => {
-  return <div>page</div>
+  return (
+    <div>
+      <Component />
+    </div>
+  )
 }
 
 export default page
