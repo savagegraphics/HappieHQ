72d910fb2742f5dc13c0fb22d5f7ad09e1111bc3
savagegraphics <olawaleraji188@gmail.com>
2024-02-10 23:14:16 -0800

    first commit

--- app/LandPage/Home/FAQ.tsx
+++ app/LandPage/Home/FAQ.tsx
@@ -1,116 +1,26 @@
-import React, { useState } from 'react'
-import { Accordion, AccordionItem } from '@nextui-org/react'
+import React from "react";
+import {Accordion, AccordionItem} from "@nextui-org/react";
 
-type Props = {}
+export default function App() {
+  const [selectedKeys, setSelectedKeys] = React.useState(new Set(["1"]));
 
-const FAQ = (props: Props) => {
-  const [selectedKeys, setSelectedKeys] = useState(new Set(['1']))
+  const defaultContent =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
 
   return (
-    <div className='max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto'>
-      <div className='grid md:grid-cols-5 gap-10 lg:m-2 m-4'>
-        <div className='md:col-span-2'>
-          <div className='max-w-xs'>
-            <h2 className='text-2xl font-bold md:text-4xl md:leading-tight dark:text-white'>
-              FAQs
-            </h2>
-            <p className='mt-1 hidden md:block text-gray-600 dark:text-gray-400'>
-              We have carefully prepared a list of the most frequent inquiries
-              for you. This will give you more insights on what Happie is about.
-            </p>
-          </div>
-        </div>
-
-        <div className='md:col-span-3'>
-          <Accordion
-            selectedKeys={selectedKeys}
-            // onSelectionChange={setSelectedKeys}
-          >
-            <AccordionItem
-              key='1'
-              aria-label='Accordion 1'
-              title={
-                <div className='font-semibold text-gray-600'>
-                  What is Happie
-                </div>
-              }
-              className='bg-gray-100 rounded-xl p-4 mb-4 mt-2'
-            >
-              <p className='text-gray-800 dark:text-gray-200'>
-                Happie, is a e-commerce logistics start-up with a mission to
-                that provide innovative, effective shipping and fulfillment
-                services for e-commerce businesses.
-              </p>
-            </AccordionItem>
-            <AccordionItem
-              key='2'
-              aria-label='Accordion 2'
-              title={
-                <div className='font-semibold text-gray-600'>
-                  Is my data secured?
-                </div>
-              }
-              className='bg-gray-100 rounded-xl p-4 mb-4 mt-2'
-            >
-              <p className='text-gray-800 dark:text-gray-200'>
-                Happie, is a e-commerce logistics start-up with a mission to
-                that provide innovative, effective shipping and fulfillment
-                services for e-commerce businesses..
-              </p>
-            </AccordionItem>
-            <AccordionItem
-              key='3'
-              aria-label='Accordion 3'
-              title={
-                <div className='font-semibold text-gray-600'>
-                  Can I be a marketer for Happie?
-                </div>
-              }
-              className='bg-gray-100 rounded-xl p-4 mb-4 mt-2'
-            >
-              <p className='text-gray-800 dark:text-gray-200'>
-                Happie, is a e-commerce logistics start-up with a mission to
-                that provide innovative, effective shipping and fulfillment
-                services for e-commerce businesses.
-              </p>
-            </AccordionItem>
-            <AccordionItem
-              key='4'
-              aria-label='Accordion 3'
-              title={
-                <div className='font-semibold text-gray-600'>
-                  Why should I use Happie for my fulfilment service?
-                </div>
-              }
-              className='bg-gray-100 rounded-xl p-4 mb-4 mt-2'
-            >
-              <p className='text-gray-800 dark:text-gray-200'>
-                Happie, is a e-commerce logistics start-up with a mission to
-                that provide innovative, effective shipping and fulfillment
-                services for e-commerce businesses.
-              </p>
-            </AccordionItem>
-            <AccordionItem
-              key='5'
-              aria-label='Accordion 3'
-              title={
-                <div className='font-semibold text-gray-600'>
-                  Do I need internet to use Happie?
-                </div>
-              }
-              className='bg-gray-100 rounded-xl p-4 mb-4 mt-2'
-            >
-              <p className='text-gray-800 dark:text-gray-200'>
-                Happie, is a e-commerce logistics start-up with a mission to
-                that provide innovative, effective shipping and fulfillment
-                services for e-commerce businesses.{' '}
-              </p>
-            </AccordionItem>
-          </Accordion>
-        </div>
-      </div>
-    </div>
-  )
+    <Accordion
+      selectedKeys={selectedKeys}
+      onSelectionChange={setSelectedKeys}
+    >
+      <AccordionItem key="1" aria-label="Accordion 1" title="Accordion 1">
+        {defaultContent}
+      </AccordionItem>
+      <AccordionItem key="2" aria-label="Accordion 2" title="Accordion 2">
+        {defaultContent}
+      </AccordionItem>
+      <AccordionItem key="3" aria-label="Accordion 3" title="Accordion 3">
+        {defaultContent}
+      </AccordionItem>
+    </Accordion>
+  );
 }
-
-export default FAQ
