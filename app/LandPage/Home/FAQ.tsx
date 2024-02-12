7946373e58459289f33a5596219d2a2b6a82397b
Toheeb Olawale RAJI <100351835+savagegraphics@users.noreply.github.com>
2024-02-12 13:53:36 -0500

    Update FAQ.tsx

--- app/LandPage/Home/FAQ.tsx
+++ app/LandPage/Home/FAQ.tsx
@@ -10,9 +10,6 @@ export default function App() {
       <AccordionItem key="1" aria-label="Accordion 1" title="Accordion 1">
         {defaultContent}
       </AccordionItem>
-      <AccordionItem key="2" aria-label="Accordion 2" title="Accordion 2">
-        {defaultContent}
-      </AccordionItem>
       <AccordionItem key="3" aria-label="Accordion 3" title="Accordion 3">
         {defaultContent}
       </AccordionItem>
