a4d4d7077b59e431fff00ff17efb1eaa4f1bd4ad
unknown <olawaleraji188@gmail.com>
2024-02-20 05:12:40 -0800

    first commit

--- app/dashboard/Deliveries/Completed.tsx
+++ app/dashboard/Deliveries/Completed.tsx
@@ -8,8 +8,8 @@ export default function App() {
     <div className="w-full flex flex-col gap-4">
       {sizes.map((size) => (
         <div key={size} className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
-          <Input size={size} type="email" label="Email" className="border-none" />
-          <Input size={size} type="email" label="Email" placeholder="Enter your email" className="border-none" />
+          <Input size={size as "sm" | "md" | "lg"} type="email" label="Email" className="border-none" />
+          <Input size={size as "sm" | "md" | "lg"} type="email" label="Email" placeholder="Enter your email" className="border-none" />
         </div>
       ))}  
     </div>  
