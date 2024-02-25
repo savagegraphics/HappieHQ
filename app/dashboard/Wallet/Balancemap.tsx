49f0442f2a3f13571bc80e37280122b2be6df6d6
unknown <olawaleraji188@gmail.com>
2024-02-24 22:01:46 -0800

    first commit

--- app/dashboard/Wallet/Balancemap.tsx
+++ app/dashboard/Wallet/Balancemap.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Selector from './Selecter/Selector'
 
 type Props = {}
 
@@ -8,11 +7,9 @@ const page = (props: Props) => {
     <div className=''>
       <div className='group relative block rounded-xl mx-8 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600'>
         {/* <BalHero /> */}
-        <div className='bg-gray-400 flex w-full h-[22rem] lg:w-[30rem] lg:h-[22rem] rounded-xl'>
+        <div className='bg-gray-400 flex w-full h-[22rem] lg:w-[32rem] lg:h-[22rem] rounded-xl'>
           <div className='flex-start'>34</div>
-          <div className='flex-end'>
-            <Selector />
-          </div>
+          <div className='flex-end'>354</div>
         </div>
       </div>
     </div>
--- app/dashboard/Wallet/page.tsx
+++ app/dashboard/Wallet/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import BalanceCard from './BalanceCard'
 import Container from './Container'
+import Selector from './Selecter/Selector'
 
 type Props = {}
 
 const page = (props: Props) => {
   return (
     <div>
+      <Selector />
       <Container />
     </div>
   )
