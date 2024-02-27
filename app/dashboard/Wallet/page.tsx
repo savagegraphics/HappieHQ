5c6da4938c6d1c285163b556cbee9d2574784561
unknown <olawaleraji188@gmail.com>
2024-02-27 08:40:52 -0800

    pey

--- app/dashboard/Wallet/page.tsx
+++ app/dashboard/Wallet/page.tsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import BalanceCard from './BalanceCard'
-import Container from './Container'
-import Selector from './Selecter/Selector'
-
-type Props = {}
-
-const page = (props: Props) => {
-  return (
-    <div>
-      <Selector />
-      <Container />
-    </div>
-  )
-}
-
-export default page
-
 // import React from 'react'
+// import BalanceCard from './BalanceCard'
+// import Container from './Container'
+// import Selector from './Selecter/Selector'
 
 // type Props = {}
 
 // const page = (props: Props) => {
-//   return <div>page</div>
+//   return (
+//     <div>
+//       <Selector />
+//       <Container />
+//     </div>
+//   )
 // }
 
 // export default page
+
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return <div>page</div>
+}
+
+export default page
