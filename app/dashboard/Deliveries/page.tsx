6693f9892839ee48f202e5b2ecf04d63435b42c3
unknown <olawaleraji188@gmail.com>
2024-02-27 17:01:57 -0800

    scratch that

--- app/dashboard/Deliveries/page.tsx
+++ app/dashboard/Deliveries/page.tsx
@@ -1,14 +1,24 @@
+// import React from 'react'
+// import Router from './Router'
+
+// type Props = {}
+
+// const page = (props: Props) => {
+//   return (
+//     <div>
+//         <Router/>
+//     </div>
+//   )
+// }
+
+// export default page
+
 import React from 'react'
-import Router from './Router'
 
 type Props = {}
 
 const page = (props: Props) => {
-  return (
-    <div>
-        <Router/>
-    </div>
-  )
+  return <div>page</div>
 }
 
 export default page
