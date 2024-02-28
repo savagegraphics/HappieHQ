c5778e15f9df0f0d08b98c7bf52380b03e174886
unknown <olawaleraji188@gmail.com>
2024-02-27 16:47:59 -0800

    scratch that

--- app/dashboard/Wallet/page.tsx
+++ app/dashboard/Wallet/page.tsx
@@ -1,21 +1,3 @@
-// import React from 'react'
-// import BalanceCard from './BalanceCard'
-// import Container from './Container'
-// import Selector from './Selecter/Selector'
-
-// type Props = {}
-
-// const page = (props: Props) => {
-//   return (
-//     <div>
-//       <Selector />
-//       <Container />
-//     </div>
-//   )
-// }
-
-// export default page
-
 import React from 'react'
 
 type Props = {}
