bf878404e0c0ec30a2a803a812a70b164da90159
unknown <olawaleraji188@gmail.com>
2024-03-03 15:45:39 -0800

    new changes

--- app/dashboard/Deliveries/page.tsx
+++ app/dashboard/Deliveries/page.tsx
@@ -1,24 +1,24 @@
-// import React from 'react'
-// import Router from './Router'
-
-// type Props = {}
-
-// const page = (props: Props) => {
-//   return (
-//     <div>
-//         <Router/>
-//     </div>
-//   )
-// }
-
-// export default page
-
 import React from 'react'
+import Router from './Router'
 
 type Props = {}
 
 const page = (props: Props) => {
-  return <div>page</div>
+  return (
+    <div>
+      <Router />
+    </div>
+  )
 }
 
 export default page
+
+// import React from 'react'
+
+// type Props = {}
+
+// const page = (props: Props) => {
+//   return <div>page</div>
+// }
+
+// export default page
