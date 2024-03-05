a2c87ad7354aa3e969c6470be4fa0b046efa9a6f
unknown <olawaleraji188@gmail.com>
2024-03-05 05:02:02 -0800

    recent changes

--- app/LandPage/Home/page.tsx
+++ app/LandPage/Home/page.tsx
@@ -14,7 +14,7 @@ type Props = {}
 const page = (props: Props) => {
   return (
     <div className='overflow-hidden'>
-      <Navbar />
+      {/* <Navbar /> */}
       <Hero />
       <Feature />
       <Card />
